e995c1ac490b90eb4b42ec03207e22aff0c1989e
Mona LisaHeyClos <thiscarlos@gmail.com>
2020-05-20 15:56:06 -0700

    single number first pass solution, needs work

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,28 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # create a place to store values
+    storage = []
 
-    pass
+    if len(arr) == 0:
+        return
+
+    # if current value already exists in storage
+    # return index of existing value
+    # delete existing(duplicate) value
+    # delete current value
+    if arr[0] in storage:
+        # find index matching current value in storage
+        duplicate_at_index = arr.index(arr[0])
+        arr.pop[duplicate_at_index]
+        single_number(arr[1:])
+
+
+    #if current value doesnt exist in storage
+    # append current value to storage
+    else:
+        storage.append(arr[0])
+        single_number(arr[1:])
 
 
 if __name__ == '__main__':
